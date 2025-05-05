9ab0d103f583464706da15b2c0504d859dc5f332
BK <dearjm04@gmail.com>
2025-05-05 17:48:39 +0700

    head favicon added

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Vinahome:Vietnam Property Platform | Find Your Perfect Home</title>
+      <meta name="description" content="Discover properties tailored for Korean expatriates in Vietnam's most popular locations" />
+      <link rel="icon" href="/favicon.png" type="image/png" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body
         suppressHydrationWarning
         className="antialiased bg-background text-foreground" // Use theme variables
